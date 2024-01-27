dd7b4297df15c5ca1d4ff673e340006d2573edfe
lordaris <audio.aris@gmail.com>
2024-01-27 14:26:55 -0600

    Modify the create course endpoint It now includes withAuth to be able to retrieve the user information from clerk and use it to create the courses with the new field createdBy

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,8 +1,39 @@
 import Courses from "@/models/courses";
 import { NextRequest, NextResponse } from "next/server";
+import { withAuth } from "@clerk/nextjs/api";
+
+// Create a course using Clerk authentication.
+export default withAuth(async function POST(request) {
+  try {
+    const { userId, sessionId, getToken } = request.session.userId;
+    if (!userId) {
+      return NextResponse.json(
+        { message: "Unauthorized", success: false },
+        { status: 401 },
+      );
+    }
+
+    const req = await request.json();
+    const { title, description } = req;
+    const newCourse = new Courses({ title, description, createdBy: userId });
+    const savedCourse = await newCourse.save();
+
+    return NextResponse.json({
+      message: "Course created successfully",
+      data: savedCourse,
+      success: true,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { message: error.message, success: false },
+      { status: 500 },
+    );
+  }
+});
 
 // Create a course. It may include sections and lessons
-export async function POST(request: NextRequest) {
+/* 
+ export async function POST(request: NextRequest) {
   try {
     const req = await request.json();
     const { title, description } = req;
@@ -24,6 +55,7 @@ export async function POST(request: NextRequest) {
     );
   }
 }
+*/
 
 export async function GET() {
   try {
