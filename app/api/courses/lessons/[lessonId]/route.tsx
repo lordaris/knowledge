b9be0b70dc9c188e30a46249e7ba01400618fd2c
lordaris <audio.aris@gmail.com>
2024-02-08 14:01:27 -0600

    Modify lessons endpoint to add PUT and DELETE methods Also modified the todo list and development ideas page.

--- app/api/courses/lessons/[lessonId]/route.tsx
+++ app/api/courses/lessons/[lessonId]/route.tsx
@@ -22,3 +22,48 @@ export async function GET(
     return NextResponse.json({ message: error.message, success: false });
   }
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { lessonId: string } },
+) {
+  try {
+    const lessonId = params.lessonId;
+    const lesson = await Lessons.findByIdAndUpdate(lessonId, request.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!lesson) {
+      return NextResponse.json(
+        {
+          message: "Lesson not found",
+        },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ data: lesson, success: true }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message, success: false });
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { lessonId: string } },
+) {
+  try {
+    const lessonId = params.lessonId;
+    const lesson = await Lessons.findByIdAndDelete(lessonId);
+    if (!lesson) {
+      return NextResponse.json(
+        {
+          message: "Lesson not found",
+        },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ data: lesson, success: true }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message, success: false });
+  }
+}
