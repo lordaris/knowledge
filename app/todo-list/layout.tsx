fa96c2f1eee9f77b6ffb302f27adcac53eea859e
lordaris <audio.aris@gmail.com>
2024-02-08 12:59:26 -0600

    Idk

---
+++ app/todo-list/layout.tsx
@@ -0,0 +1,11 @@
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <article className="prose flex max-w-full flex-col items-center justify-center py-4 ">
+      <div className="w-3/4">{children}</div>
+    </article>
+  );
+}
